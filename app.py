1660cd80da1a4c34c6782c2939879d2833496739
Sohamchandekar <132808498+Sohamchandekar@users.noreply.github.com>
2024-06-14 11:47:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -343,10 +343,12 @@ def login_to_maharerait(user_id, password):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")  # Enable remote debugging
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
 
     driver = webdriver.Chrome(options=chrome_options)
+    
     try:
         driver.get("https://maharerait.mahaonline.gov.in/")
         username_field = WebDriverWait(driver, 20).until(
@@ -441,4 +443,7 @@ def json_generator(input_file):
     # Save JSON data to a file
     with open('ProjectsCheckData.json', 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
 
